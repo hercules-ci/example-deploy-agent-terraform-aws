360be0a854fc1312ba7bdbcbdb3f397a97981f7e
Robert Hensing <robert@roberthensing.nl>
2020-08-27 16:15:35 +0200

    Remove hercules_ci_agent_aws indirection
    That module was only suitable for simple examples and switching
    away from it is work.
    Removing the indirection makes this example easier to adapt.

--- main.tf
+++ main.tf
@@ -16,23 +16,72 @@ provider "aws" {
   # configure credentials here and in s3 backend above
 }
 
-module "agent-1" {
-  source = "git::https://github.com/hercules-ci/terraform-hercules-ci.git//hercules_ci_agent_aws?ref=6880d40e1a2745096d80d9c5b1c659c0f6b9768c"
+locals {
+  public_key = "~/.ssh/id_rsa.pub"
+  instance_type = "t3.medium"
+  disk_size = 50 # in GiB
+}
+
+module "nixos" {
+  source = "git::https://github.com/hercules-ci/terraform-hercules-ci.git//hercules_ci_agent_nixos?ref=c3c7e2155b7e872cab52460faa86a2fb0cd60375"
+  target_host = aws_instance.machine.public_ip
   use_prebuilt = true
+  configs = [abspath("${path.module}/configuration.nix")]
   cluster_join_token = "${file("${path.module}/cluster-join-token.key")}"
   binary_caches_json = "${file("${path.module}/binary-caches.json")}"
+  NIX_PATH = "nixpkgs=${jsondecode(file("${path.module}/nix/sources.json"))["nixpkgs"]["url"]}"
 
   # This is not a great solution.
   # Also, run `ssh-add ~/.ssh/id_rsa`
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  ssh_private_key_file = "~/.ssh/id_rsa"
+  ssh_agent = true
 
-  configs = [
-    abspath("${path.module}/extra-configuration.nix"),
-  ]
+  triggers = {
+    machine_id = aws_instance.machine.id
+    a = 1
+  }
+}
+
+module "nixos_image_1909" {
+  source = "git::https://github.com/hercules-ci/terraform-nixos.git//aws_image_nixos?ref=65fc5758a6660386a02ab32d9e7245cd9a521445"
+  release = "20.03"
+}
+
+resource "aws_security_group" "ssh_and_egress" {
+  ingress {
+    # SSH for deployment
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ] # TODO: restrict
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key-${sha256(file(local.public_key))}"
+  public_key = file(local.public_key)
+}
+
+resource "aws_instance" "machine" {
+  ami           = module.nixos_image_1909.ami
+  instance_type = local.instance_type
+  security_groups = [ aws_security_group.ssh_and_egress.name ]
+  key_name = aws_key_pair.deployer.key_name
+  
+  root_block_device {
+    volume_size = local.disk_size # GiB
+  }
 }
 
 output "public_dns" {
-  value = "${module.agent-1.public_dns}"
+  value = aws_instance.machine.public_dns
 }
 
 ################
@@ -61,7 +110,7 @@ resource "aws_dynamodb_table" "terraform_state" {
     type = "S"
   }
 
-  tags {
+  tags = {
     Name = "Terraform Lock Table"
   }
 }
